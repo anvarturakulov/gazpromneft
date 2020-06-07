18f97dba885dd65f9a54b6fe701ff59474fe2f15
anvarturakulov <anvarsam@mail.ru>
2020-06-07 14:29:27 +0500

    versta + adaptiv 1000px + adaptiv na 640px

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,18 @@ $(function(){
     $('.btn').on('click',function(){
         document.getElementById('input_login').value = "";
     });
+    
+    $('select').wSelect();
+
+    $('#lang').change(function() {
+      console.log($(this).val());
+    });
+
+    $('#lang').val('RUS').change(); // should see in console
+    $('#lang').wSelect('reset');
+
+    $('.menu__btn').on('click',function(){
+      $('.header__menu').slideToggle();
+    });
+
 });
